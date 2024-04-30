2a89380fdde6890c53651f381e75d30b25bc3c22
Hibba <hibbaafzal299@gmail.com>
2024-04-30 11:21:45 -0400

    made cases

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -1,17 +1,18 @@
 package com.ps;
 
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
 
-    ArrayList<String> str = new ArrayList<String>();
+    static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
 
 
         int commandHomeScreen;
-// home screen
+
         do {
+            // asking user.
             System.out.println("Welcome to the Accounting Ledger Application!");
             System.out.println("What would you like to do?");
             System.out.println("\t[1] Add Deposit");
@@ -19,91 +20,73 @@ public class Main {
             System.out.println("\t[3] Ledger");
             System.out.println("\t[4] Exit Application");
 
-            // Creating an instance of scanner.
-            Scanner scanner = new Scanner(System.in);
-
             commandHomeScreen = scanner.nextInt();
 
-
-            // Switch statements for different user commands
             switch (commandHomeScreen) {
                 case 1:
-                    // Collect information for a deposit.
-                    // String of arrays called depositInformationFromUser.
+                    //array of strings called collected deposit information from user
+                    String[] collectedDepositInformationFromUser = information(scanner);
 
-                    //   fix this        //    String[] depositInformationFromUser = informationFromUser(scanner);
-                    //  if (depositInformationFromUser != null) {
-
-                    // static method in ledger that adds the deposit information from the user.
-// add here
-                {
-                }
-                break;
-
-
-                case 2:
-                    // Collect information for a payment
-                    // String of arrays called paymentInformationFromUser.
-                    String[] paymentInformationFromUser = informationFromUser(scanner);
-                    if (paymentInformationFromUser != null) {
-
-                        // static method in class Ledger called add payment information
+                    if (collectedDepositInformationFromUser != null) {
+                        // Static method in class ledger named add deposit information.
+                        Ledger.addDepositInformation(collectedDepositInformationFromUser);
                     }
-                    //   Ledger.addPaymentInformation(paymentInformationFromUser); // not sure about this: fix it
-
                     break;
+                case 2:
+                    //array of strings called collected payment information from user
+                    String[] collectedPaymentInformationFromUser = information(scanner);
 
-
+                    if (collectedPaymentInformationFromUser != null) {
+                        // Static method in class ledger named add payment information.
+                        Ledger.addPaymentInformation(collectedPaymentInformationFromUser);
+                    }
+                    break;
                 case 3:
-                    // Display the ledger to the user
                     System.out.println("Displaying Ledger. Please Wait...");
-                    // static method in class Ledger called homePage.
+                    // Static method in class ledger named add home page.
                     Ledger.homePage(scanner);
 
                     break;
-
                 case 4:
-                    // Exit the application
 
                     System.out.println("Exiting...");
-
+                    System.out.println("Thank you for banking with us!");
+                    System.out.println("Have a nice day!");
                     break;
 
                 default:
-                    // invalid commands
-                    System.out.println("Invalid. Please enter a valid input.");
-                    break;
+                    System.out.println("This is an invalid input. Please enter a valid input.");
             }
-
         } while (commandHomeScreen != 4);
 
 
     }
 
 
-    // Static method to collect transaction information from the user
+// collecting information from the user about the transactions.
 
-    public static String[] informationFromUser(Scanner scanner) {
-        System.out.print("Enter item description: ");
-        String itemDescription = scanner.nextLine();
+    public static String[] information (Scanner scanner) {
+        System.out.print("Please enter item description: ");
+        String description = scanner.nextLine();
         scanner.nextLine();
 
-        System.out.print("Enter vendor: ");
-        String vendorInformation = scanner.nextLine();
+        System.out.print("Please enter the vendor: ");
+        String vendor = scanner.nextLine();
 
-        System.out.print("Enter dollar amount: $");
-        double dollarAmount = scanner.nextDouble();
-        scanner.nextLine();
+        System.out.print("Please enter the dollar amount: $");
 
-        // returning the information the user had provided in a new array.
-        return new String[]{itemDescription, vendorInformation, Double.toString(dollarAmount)};
+        // checks to see if the user put a double value or not.
+        if (scanner.hasNextDouble()) {
+            double dollarAmount = scanner.nextDouble();
+            scanner.nextLine();
 
-
+            return new String[]{description, vendor, Double.toString(dollarAmount)};
+        } else {
+            scanner.nextLine();
+            System.out.println("\nThe input type you have entered is incorrect. Please try again.");
+            return null;
+        }
     }
+
+
 }
-
-
-
-
-
-
